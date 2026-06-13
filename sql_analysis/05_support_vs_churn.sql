878b4bbf2985fb598eb3cd02eac720d84f7c5313
Suraj Rajput <109722721+SURAJRAJPUT2006@users.noreply.github.com>
2026-06-13 11:53:46 +0530

    Revise insights on churn risks and recommendations
    Updated insights and recommendations regarding churn analysis.

--- sql_analysis/05_support_vs_churn.sql
+++ sql_analysis/05_support_vs_churn.sql
@@ -51,7 +51,7 @@ SELECT
   SUM(a.churn_flag) AS churned_users,
   ROUND(SUM(a.churn_flag)* 100.0 / COUNT(*),2) AS churn_pct
 FROM accounts a
-LEFT JOIN resolution_bucket rb
+LEFT JOIN resolution_bucket rb 
 ON rb.account_id = a.account_id
 GROUP BY 1 
 ORDER BY churn_pct DESC;
@@ -88,19 +88,21 @@ FROM satisfaction_affected_table
 -----------------------------------------------------------------------------------------------------------------------------------
 -- Insight :
 /*
-A. The "CSAT Smoking Gun" (55 Churned)
-Result: 47 people churned within 30 days of a bad score.
-Insight: This is your leading indicator. A bad score isn't just "unhappy feedback"; it is a cancellation notice 30 days in advance.
-Recommendation: Any satisfaction score < 3 must trigger an Auto-Escalation to a manager. You have 30 days to save that revenue.
 
-B. The "Low Engagement" Risk (13.46% Churn)
+A. The "Low Engagement" Risk (13.46% Churn)
 Result: The "1-2 tickets" group has higher churn than the "6+ tickets" group.
 Insight: High support volume is actually a sign of HEALTH. It means they are using the product and trying to make it work. 
 Customers with only 1 ticket are "unengaged." They run into one problem, get bored, and leave.
 
-C. The "SLA" Reality (48 Hours)
-Result: Your 48h analysis shows 11.86% (High Res time) vs 9.81% (Less Res time).
+B. The "SLA" Reality (48 Hours)
+Result:  48h analysis shows 11.86% (High Res time) vs 9.81% (Less Res time).
 Insight: This proves the ROI of speed. Moving a customer from the "High" bucket to the "Less" bucket reduces their 
 churn probability by ~2%.
 Impact: If those 1,627 users had 2% lower churn, we would save 32 Enterprise/Pro accounts.
+
+C. The "CSAT Smoking Gun" 
+Result: 47 people churned within 30 days of a bad score.
+Insight: This is your leading indicator. A bad score isn't just "unhappy feedback"; it is a cancellation notice 30 days in advance.
+Recommendation: Any satisfaction score < 3 must trigger an Auto-Escalation to a manager. You have 30 days to save that revenue.
+
 */
